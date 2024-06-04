1d745cac437aa50e7c5f2ee7e11517f554b75aa4
Jonathan Gan <jonathang132298@gmail.com>
2024-06-04 02:13:21 -0400

    I hate discord

--- app/linkpage.tsx
+++ app/linkpage.tsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import './App.css';
 import LinkTree from './components/LinkTree';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
 
   return (
     <>
       <LinkTree />
+      <Analytics/>
     </>
   );
 }
