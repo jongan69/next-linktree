969ca064362e3fbfa5b39c4b270a02c3ff0d01af
Jonathan Gan <jonathang132298@gmail.com>
2025-01-19 09:29:02 -0500

    Removed whitespace

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-01dacce630ff865c.js","static/chunks/fd9d1056-09e3f0a3163b45af.js","static/chunks/117-fa869a5a1c3d0073.js","static/chunks/main-app-16cb2a8151345cad.js"],pages:{"/_app":["static/chunks/webpack-01dacce630ff865c.js","static/chunks/framework-ecc7c29b98f29b59.js","static/chunks/main-0bb028e0f36edf41.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-01dacce630ff865c.js","static/chunks/framework-ecc7c29b98f29b59.js","static/chunks/main-0bb028e0f36edf41.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- app/components/Three/Background.tsx
+++ app/components/Three/Background.tsx
@@ -20,7 +20,7 @@ export default function Background() {
   }, []);
 
   return (
-    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1 }
+    <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', zIndex: -1, backgroundColor: '#1a1a1a' }
     }>
       {hasWebGL ? <Canvas>
         < RainingMoneyBackground />
