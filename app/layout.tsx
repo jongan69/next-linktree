3a270a9fcec0488477d5a2e6721bd8156b8692f8
Jonathan Gan <jonathang132298@gmail.com>
2025-03-20 14:54:20 -0400

    ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6202902142885850"
+     crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
           {children}
       </body>
